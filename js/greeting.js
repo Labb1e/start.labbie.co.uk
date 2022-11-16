748a4c8a6ee8e5bae1724f139c6b2c2c472e3f92
Labb1e <105300489+Labb1e@users.noreply.github.com>
2022-11-16 12:21:31 +0000

    changed title to update with greeting

--- js/greeting.js
+++ js/greeting.js
@@ -16,12 +16,17 @@ var gree6 = 'Good evening';
 // Define the hours of the greetings
 if (Hr >= 23 && Hr < 5) {
     document.getElementById('greetings').innerText = gree1;
+    document.title = gree1
 } else if (Hr >= 6 && Hr < 12) {
     document.getElementById('greetings').innerText = gree2 ;
+    document.title = gree2
 } else if (Hr >= 12 && Hr < 17) {
     document.getElementById('greetings').innerText = gree3;
+    document.title = gree3
 } else if (Hr >= 17 && Hr < 22) {
     document.getElementById('greetings').innerText = gree4;
+    document.title = gree4
 } else if (Hr >= 22 && Hr < 23) {
     document.getElementById('greetings').innerText = gree5;
+    document.title = gree5
 }
